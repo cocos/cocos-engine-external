44028cb51b7721db8a60a3ae1145ef22e178116e
Wenhai Lin <wenhai.lin@chukong-inc.com>
2016-03-29 23:27:43 +0800

    cocos 2d lite external

---
+++ android/Android.mk
@@ -0,0 +1,98 @@
+LOCAL_PATH := $(call my-dir)
+#======================================
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := cocos_zlib_static
+LOCAL_MODULE_FILENAME := zlib
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libz.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+#======================================
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := cocos_freetype2_static
+LOCAL_MODULE_FILENAME := freetype2
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libfreetype.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include/freetype
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+#======================================
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := cocos_jpeg_static
+LOCAL_MODULE_FILENAME := jpeg
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libjpeg.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+#======================================
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := cocos_png_static
+LOCAL_MODULE_FILENAME := png
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libpng.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+#======================================
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := cocos_chipmunk_static
+LOCAL_MODULE_FILENAME := chipmunk
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libchipmunk.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+#======================================
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := cocos_tiff_static
+LOCAL_MODULE_FILENAME := tiff
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libtiff.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+#======================================
+#include $(CLEAR_VARS)
+#
+#LOCAL_MODULE := cocos_webp_static
+#LOCAL_MODULE_FILENAME := webp
+#LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libwebp.a
+#
+#LOCAL_WHOLE_STATIC_LIBRARIES := cpufeatures
+#
+#ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+#    LOCAL_CFLAGS := -DHAVE_NEON=1
+#endif
+#
+#include $(PREBUILT_STATIC_LIBRARY)
+#
+#======================================
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := websockets_static
+LOCAL_MODULE_FILENAME := libwebsockets_static
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libwebsockets.a
+
+LOCAL_CPPFLAGS := -D__STDC_LIMIT_MACROS=1
+LOCAL_EXPORT_CPPFLAGS := -D__STDC_LIMIT_MACROS=1
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+#======================================
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := spidermonkey_static
+LOCAL_MODULE_FILENAME := js_static
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libjs_static.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include/spidermonkey
+
+LOCAL_CPPFLAGS := -D__STDC_LIMIT_MACROS=1 -Wno-invalid-offsetof
+LOCAL_EXPORT_CPPFLAGS := -D__STDC_LIMIT_MACROS=1 -Wno-invalid-offsetof
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+#======================================
+#$(call import-module,android/cpufeatures)
