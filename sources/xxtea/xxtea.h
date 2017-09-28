9638089278fc5f8b6d413ef3165d2dd13836d1a6
2youyou2 <501251991@qq.com>
2017-09-28 17:24:06 +0800

    define XXTEA_DLL macro for xxtea

--- sources/xxtea/xxtea.h
+++ sources/xxtea/xxtea.h
@@ -20,7 +20,20 @@
 #define XXTEA_H
 
 #include <stddef.h> /* for size_t & NULL declarations */
-#include "platform/CCPlatformMacros.h"
+
+#if CC_TARGET_PLATFORM == CC_PLATFORM_WIN32
+#if defined(CC_STATIC)
+#define XXTEA_DLL
+#else
+#if defined(_USRDLL)
+#define XXTEA_DLL     __declspec(dllexport)
+#else         /* use a DLL library */
+#define XXTEA_DLL     __declspec(dllimport)
+#endif
+#endif
+#else
+#define XXTEA_DLL
+#endif
 
 #ifdef __cplusplus
 extern "C" {
@@ -46,8 +59,8 @@ typedef uint32_t xxtea_long;
 #define XXTEA_MX (z >> 5 ^ y << 2) + (y >> 3 ^ z << 4) ^ (sum ^ y) + (k[p & 3 ^ e] ^ z)
 #define XXTEA_DELTA 0x9e3779b9
 
-CC_DLL unsigned char *xxtea_encrypt(unsigned char *data, xxtea_long data_len, unsigned char *key, xxtea_long key_len, xxtea_long *ret_length);
-CC_DLL unsigned char *xxtea_decrypt(unsigned char *data, xxtea_long data_len, unsigned char *key, xxtea_long key_len, xxtea_long *ret_length);
+XXTEA_DLL unsigned char *xxtea_encrypt(unsigned char *data, xxtea_long data_len, unsigned char *key, xxtea_long key_len, xxtea_long *ret_length);
+XXTEA_DLL unsigned char *xxtea_decrypt(unsigned char *data, xxtea_long data_len, unsigned char *key, xxtea_long key_len, xxtea_long *ret_length);
 
 #endif
 
