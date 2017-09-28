808c57d01be3d52a6680f1c2da2900280ce9df60
2youyou2 <501251991@qq.com>
2017-09-28 11:02:01 +0800

    export xxtea functions to DLL

--- sources/xxtea/xxtea.h
+++ sources/xxtea/xxtea.h
@@ -20,6 +20,11 @@
 #define XXTEA_H
 
 #include <stddef.h> /* for size_t & NULL declarations */
+#include "platform/CCPlatformMacros.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #if defined(_MSC_VER)
 
@@ -41,7 +46,12 @@ typedef uint32_t xxtea_long;
 #define XXTEA_MX (z >> 5 ^ y << 2) + (y >> 3 ^ z << 4) ^ (sum ^ y) + (k[p & 3 ^ e] ^ z)
 #define XXTEA_DELTA 0x9e3779b9
 
-unsigned char *xxtea_encrypt(unsigned char *data, xxtea_long data_len, unsigned char *key, xxtea_long key_len, xxtea_long *ret_length);
-unsigned char *xxtea_decrypt(unsigned char *data, xxtea_long data_len, unsigned char *key, xxtea_long key_len, xxtea_long *ret_length);
+CC_DLL unsigned char *xxtea_encrypt(unsigned char *data, xxtea_long data_len, unsigned char *key, xxtea_long key_len, xxtea_long *ret_length);
+CC_DLL unsigned char *xxtea_decrypt(unsigned char *data, xxtea_long data_len, unsigned char *key, xxtea_long key_len, xxtea_long *ret_length);
 
+#endif
+
+
+#ifdef __cplusplus
+}
 #endif
