b608028dacc0dc972a5143ddd014b60fe564d0d9
James Chen <jianhua.chen@cocos2d-x.org>
2017-10-10 12:38:34 +0800

    Adds libuv for android

--- android/Android.mk
+++ android/Android.mk
@@ -173,6 +173,15 @@ include $(PREBUILT_STATIC_LIBRARY)
 #======================================
 include $(CLEAR_VARS)
 
+LOCAL_MODULE := uv_static
+LOCAL_MODULE_FILENAME := libuv
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libuv.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include/uv
+include $(PREBUILT_STATIC_LIBRARY)
+
+#======================================
+include $(CLEAR_VARS)
+
 LOCAL_MODULE := PluginProtocolStatic
 LOCAL_MODULE_FILENAME := libPluginProtocolStatic
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libPluginProtocolStatic.a
