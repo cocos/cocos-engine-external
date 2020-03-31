ab14746a5649e0bf61946c21798237f69eeedff5
Arnold <397136899@qq.com>
2020-03-31 14:11:29 +0800

    update source code

--- win32/include/jpeg/jmorecfg.h
+++ win32/include/jpeg/jmorecfg.h
@@ -263,19 +263,6 @@ typedef void noreturn_t;
 typedef enum { FALSE = 0, TRUE = 1 } boolean;
 #endif
 
-
-#if _WIN32
-
-#  ifdef FALSE			/* in case these macros already exist */
-#    undef FALSE			/* values of boolean */
-#  endif
-#  ifdef TRUE
-#    undef TRUE	
-#  endif
-
-typedef enum { FALSE = 0, TRUE = 1 } boolean;
-#endif
-
 /*
  * The remaining options affect code selection within the JPEG library,
  * but they don't need to be visible to most applications using the library.
