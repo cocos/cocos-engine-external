4fe69eb30738761765a8894e51e2d77317faab00
minggo <nudtzxm@gmail.com>
2020-08-27 16:46:55 +0800

    Update tinyxml2.h

--- sources/tinyxml2/tinyxml2.h
+++ sources/tinyxml2/tinyxml2.h
@@ -24,7 +24,7 @@ distribution.
 #ifndef TINYXML2_INCLUDED
 #define TINYXML2_INCLUDED
 
-#include "base/ccMacros.h"
+#include "base/Macros.h"
 
 #if defined(ANDROID_NDK) || defined(__BORLANDC__) || (CC_TARGET_PLATFORM == CC_PLATFORM_BLACKBERRY)
 #   include <ctype.h>
