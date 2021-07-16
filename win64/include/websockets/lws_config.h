8a52b672bf0498b533a04bc462d6541a3ea35829
timlyeee <43355299+timlyeee@users.noreply.github.com>
2021-07-16 11:12:25 +0800

    Update websocket libs and so on (#161)
    * version of lib websocket has been return to old version 2.4.2
    * freetype lib has been confirmed as static lib
    * delete unused librarys ( webpdecoder.dll, vorbisenc.dll etc )
    * rename uv dir as the old version.

--- win64/include/websockets/lws_config.h
+++ win64/include/websockets/lws_config.h
@@ -6,193 +6,151 @@
 	#endif
 #endif
 
-#define LWS_INSTALL_DATADIR "E:/ProjectsInLearn/vcpkg/packages/libwebsockets_x64-windows/share"
-#define LWS_INSTALL_LIBDIR "E:/ProjectsInLearn/vcpkg/packages/libwebsockets_x64-windows/lib"
-#define LWS_LIBRARY_VERSION_MAJOR 4
-#define LWS_LIBRARY_VERSION_MINOR 1
-#define LWS_LIBRARY_VERSION_PATCH_ELABORATED 6-2021.05.12-122-g835f1613b
-#define LWS_LIBRARY_VERSION_PATCH 6
+#define LWS_INSTALL_DATADIR "C:/Program Files/libwebsockets/share"
 
+/* Define to 1 to use wolfSSL/CyaSSL as a replacement for OpenSSL.
+ * LWS_OPENSSL_SUPPORT needs to be set also for this to work. */
+/* #undef USE_WOLFSSL */
+
+/* Also define to 1 (in addition to USE_WOLFSSL) when using the
+  (older) CyaSSL library */
+/* #undef USE_OLD_CYASSL */
+/* #undef LWS_WITH_BORINGSSL */
+
+/* #undef LWS_WITH_MBEDTLS */
+/* #undef LWS_WITH_POLARSSL */
+/* #undef LWS_WITH_ESP8266 */
+/* #undef LWS_WITH_ESP32 */
+
+/* #undef LWS_WITH_PLUGINS */
+/* #undef LWS_WITH_NO_LOGS */
+
+/* The Libwebsocket version */
+#define LWS_LIBRARY_VERSION "2.4.2"
+
+#define LWS_LIBRARY_VERSION_MAJOR 2
+#define LWS_LIBRARY_VERSION_MINOR 4
+#define LWS_LIBRARY_VERSION_PATCH 2
 /* LWS_LIBRARY_VERSION_NUMBER looks like 1005001 for e.g. version 1.5.1 */
-#define LWS_LIBRARY_VERSION_NUMBER (LWS_LIBRARY_VERSION_MAJOR * 1000000) + \
-					(LWS_LIBRARY_VERSION_MINOR * 1000) + \
-					LWS_LIBRARY_VERSION_PATCH
+#define LWS_LIBRARY_VERSION_NUMBER (LWS_LIBRARY_VERSION_MAJOR*1000000)+(LWS_LIBRARY_VERSION_MINOR*1000)+LWS_LIBRARY_VERSION_PATCH
+
+/* The current git commit hash that we're building from */
+#define LWS_BUILD_HASH "desktop-kmeijnm\\administrator@DESKTOP-KMEIJNM-v2.0.0-640-g8964ce9d"
+
+/* Build with OpenSSL support */
+#define LWS_OPENSSL_SUPPORT
+
+/* The client should load and trust CA root certs it finds in the OS */
+#define LWS_SSL_CLIENT_USE_OS_CA_CERTS
+
+/* Sets the path where the client certs should be installed. */
+#define LWS_OPENSSL_CLIENT_CERTS "../share"
+
+/* Turn off websocket extensions */
+/* #undef LWS_NO_EXTENSIONS */
+
+/* Enable libev io loop */
+/* #undef LWS_WITH_LIBEV */
+
+/* Enable libuv io loop */
+#define LWS_WITH_LIBUV
+
+/* Enable libevent io loop */
+/* #undef LWS_WITH_LIBEVENT */
+
+/* Build with support for ipv6 */
+/* #undef LWS_WITH_IPV6 */
+
+/* Build with support for UNIX domain socket */
+/* #undef LWS_WITH_UNIX_SOCK */
+
+/* Build with support for HTTP2 */
+/* #undef LWS_WITH_HTTP2 */
+
+/* Turn on latency measuring code */
+/* #undef LWS_LATENCY */
+
+/* Don't build the daemonizeation api */
+#define LWS_NO_DAEMONIZE
+
+/* Build without server support */
+/* #undef LWS_NO_SERVER */
+
+/* Build without client support */
+/* #undef LWS_NO_CLIENT */
+
+/* If we should compile with MinGW support */
+/* #undef LWS_MINGW_SUPPORT */
+
+/* Use the BSD getifaddrs that comes with libwebsocket, for uclibc support */
+#define LWS_BUILTIN_GETIFADDRS
+
+/* use SHA1() not internal libwebsockets_SHA1 */
+/* #undef LWS_SHA1_USE_OPENSSL_NAME */
+
+/* SSL server using ECDH certificate */
+/* #undef LWS_SSL_SERVER_WITH_ECDH_CERT */
+/* #define LWS_HAVE_SSL_CTX_set1_param */
+#define LWS_HAVE_X509_VERIFY_PARAM_set1_host
+#define LWS_HAVE_RSA_SET0_KEY
+
+/* #undef LWS_HAVE_UV_VERSION_H */
+
+/* CGI apis */
+/* #undef LWS_WITH_CGI */
+
+/* whether the Openssl is recent enough, and / or built with, ecdh */
+#define LWS_HAVE_OPENSSL_ECDH_H
+
+/* HTTP Proxy support */
+/* #undef LWS_WITH_HTTP_PROXY */
+
+/* HTTP Ranges support */
+#define LWS_WITH_RANGES
+
+/* Http access log support */
+/* #undef LWS_WITH_ACCESS_LOG */
+/* #undef LWS_WITH_SERVER_STATUS */
+
+/* #undef LWS_WITH_STATEFUL_URLDECODE */
+/* #undef LWS_WITH_PEER_LIMITS */
+
+/* Maximum supported service threads */
 #define LWS_MAX_SMP 1
 
-/* #undef LWS_ESP_PLATFORM */
-/* #undef LWS_LIBRARY_VERSION_NUMBER */
+/* Lightweight JSON Parser */
+/* #undef LWS_WITH_LEJP */
 
-#define LWS_EXT_PTHREAD_LIBRARIES
+/* SMTP */
+/* #undef LWS_WITH_SMTP */
+
+/* OPTEE */
+/* #undef LWS_PLAT_OPTEE */
+
+/* ZIP FOPS */
+#define LWS_WITH_ZIP_FOPS
+#define LWS_HAVE_STDINT_H
 
 /* #undef LWS_AVOID_SIGPIPE_IGN */
-#define LWS_BUILD_HASH "2021.05.12-122-g835f1613b"
-#define LWS_BUILTIN_GETIFADDRS
-#define LWS_CLIENT_HTTP_PROXYING
-/* #undef LWS_DETECTED_PLAT_IOS */
-/* #undef LWS_FALLBACK_GETHOSTBYNAME */
-#define LWS_HAS_INTPTR_T
-/* #undef LWS_HAS_GETOPT_LONG */
-#define LWS_HAVE__ATOI64
-#define LWS_HAVE_ATOLL
-#define LWS_HAVE_BN_bn2binpad
-/* #undef LWS_HAVE_CLOCK_GETTIME */
-#define LWS_HAVE_EC_POINT_get_affine_coordinates
-#define LWS_HAVE_ECDSA_SIG_set0
-#define LWS_HAVE_EVP_MD_CTX_free
-#define LWS_HAVE_EVP_aes_128_wrap
-#define LWS_HAVE_EVP_aes_128_cfb8
-#define LWS_HAVE_EVP_aes_128_cfb128
-#define LWS_HAVE_EVP_aes_192_cfb8
-#define LWS_HAVE_EVP_aes_192_cfb128
-#define LWS_HAVE_EVP_aes_256_cfb8
-#define LWS_HAVE_EVP_aes_256_cfb128
-#define LWS_HAVE_EVP_aes_128_xts
-#define LWS_HAVE_EVP_PKEY_new_raw_private_key
-#define LWS_HAVE_EXECVPE
-/* #undef LWS_HAVE_LIBCAP */
-#define LWS_HAVE_HMAC_CTX_new
-#define LWS_HAVE_MALLOC_H
-/* #undef LWS_HAVE_MALLOC_TRIM */
-/* #undef LWS_HAVE_MALLOC_USABLE_SIZE */
-/* #undef LWS_HAVE_mbedtls_md_setup */
-/* #undef LWS_HAVE_mbedtls_net_init */
-/* #undef LWS_HAVE_mbedtls_rsa_complete */
-/* #undef LWS_HAVE_mbedtls_internal_aes_encrypt */
-/* #undef LWS_HAVE_mbedtls_ssl_conf_alpn_protocols */
-/* #undef LWS_HAVE_mbedtls_ssl_get_alpn_protocol */
-/* #undef LWS_HAVE_mbedtls_ssl_conf_sni */
-/* #undef LWS_HAVE_mbedtls_ssl_set_hs_ca_chain */
-/* #undef LWS_HAVE_mbedtls_ssl_set_hs_own_cert */
-/* #undef LWS_HAVE_mbedtls_ssl_set_hs_authmode */
-/* #undef LWS_HAVE_MBEDTLS_NET_SOCKETS */
-#define LWS_HAVE_NEW_UV_VERSION_H
-#define LWS_HAVE_OPENSSL_ECDH_H
-/* #undef LWS_HAVE_PIPE2 */
-/* #undef LWS_HAVE_EVENTFD */
-#define LWS_HAVE_PTHREAD_H
-#define LWS_HAVE_RSA_SET0_KEY
-/* #undef LWS_HAVE_RSA_verify_pss_mgf1 */
-#define LWS_HAVE_SSL_CTX_get0_certificate
-/* #undef LWS_HAVE_SSL_CTX_load_verify_file */
-/* #undef LWS_HAVE_SSL_CTX_load_verify_dir */
-#define LWS_HAVE_SSL_CTX_set1_param
-#define LWS_HAVE_SSL_CTX_set_ciphersuites
-#define LWS_HAVE_SSL_EXTRA_CHAIN_CERTS
-#define LWS_HAVE_SSL_get0_alpn_selected
-#define LWS_HAVE_SSL_CTX_EVP_PKEY_new_raw_private_key
-#define LWS_HAVE_SSL_set_alpn_protos
-#define LWS_HAVE_SSL_SET_INFO_CALLBACK
-#define LWS_HAVE__STAT32I64
-#define LWS_HAVE_STDINT_H
+
+#define LWS_FALLBACK_GETHOSTBYNAME
+
+/* #undef LWS_WITH_STATS */
+/* #undef LWS_WITH_SOCKS5 */
+
 /* #undef LWS_HAVE_SYS_CAPABILITY_H */
+/* #undef LWS_HAVE_LIBCAP */
+
+#define LWS_HAVE_ATOLL
+#define LWS_HAVE__ATOI64
+#define LWS_HAVE__STAT32I64
+
+/* OpenSSL various APIs */
+
 #define LWS_HAVE_TLS_CLIENT_METHOD
 #define LWS_HAVE_TLSV1_2_CLIENT_METHOD
-/* #undef LWS_HAVE_UV_VERSION_H */
-/* #undef LWS_HAVE_VFORK */
-#define LWS_HAVE_X509_get_key_usage
-#define LWS_HAVE_X509_VERIFY_PARAM_set1_host
-#define LWS_LIBRARY_VERSION "4.1.6-2021.05.12-122-g835f1613b"
-#define LWS_LOGGING_BITFIELD_CLEAR 0
-#define LWS_LOGGING_BITFIELD_SET 0
-/* #undef LWS_MINGW_SUPPORT */
-/* #undef LWS_NO_CLIENT */
-#define LWS_NO_DAEMONIZE
-#define LWS_OPENSSL_CLIENT_CERTS "../share"
-#define LWS_OPENSSL_SUPPORT
-/* #undef LWS_PLAT_OPTEE */
-/* #undef LWS_PLAT_UNIX */
-/* #undef LWS_PLAT_FREERTOS */
-/* #undef LWS_ROLE_CGI */
-/* #undef LWS_ROLE_DBUS */
-#define LWS_ROLE_H1
-#define LWS_ROLE_H2
-#define LWS_ROLE_RAW
-#define LWS_ROLE_RAW_FILE
-/* #undef LWS_ROLE_RAW_PROXY */
-#define LWS_ROLE_WS
-/* #undef LWS_ROLE_MQTT */
-/* #undef LWS_SHA1_USE_OPENSSL_NAME */
-#define LWS_SSL_CLIENT_USE_OS_CA_CERTS
-/* #undef LWS_SSL_SERVER_WITH_ECDH_CERT */
-/* #undef LWS_WITH_ABSTRACT */
-/* #undef LWS_WITH_ACCESS_LOG */
-/* #undef LWS_WITH_ACME */
-/* #undef LWS_WITH_ALSA */
-/* #undef LWS_WITH_SYS_ASYNC_DNS */
-/* #undef LWS_WITH_BORINGSSL */
-/* #undef LWS_WITH_CGI */
-#define LWS_WITH_CUSTOM_HEADERS
-/* #undef LWS_WITH_DEPRECATED_LWS_DLL */
-/* #undef LWS_WITH_DETAILED_LATENCY */
-#define LWS_WITH_DIR
-/* #undef LWS_WITH_DRIVERS */
-/* #undef LWS_WITH_ESP32 */
-/* #undef LWS_HAVE_EVBACKEND_LINUXAIO */
-/* #undef LWS_HAVE_EVBACKEND_IOURING */
-/* #undef LWS_WITH_EXTERNAL_POLL */
-#define LWS_WITH_FILE_OPS
-/* #undef LWS_WITH_FSMOUNT */
-/* #undef LWS_WITH_FTS */
-#define LWS_WITH_GENCRYPTO
-/* #undef LWS_WITH_GENERIC_SESSIONS */
-/* #undef LWS_WITH_GLIB */
-/* #undef LWS_WITH_GTK */
-#define LWS_WITH_HTTP2
-#define LWS_WITH_HTTP_BASIC_AUTH
-/* #undef LWS_WITH_HTTP_BROTLI */
-/* #undef LWS_HTTP_HEADERS_ALL */
-/* #undef LWS_WITH_HTTP_PROXY */
-#define LWS_WITH_HTTP_STREAM_COMPRESSION
-#define LWS_WITH_HTTP_UNCOMMON_HEADERS
-#define LWS_WITH_IPV6
-/* #undef LWS_WITH_JOSE */
-#define LWS_WITH_LEJP
-/* #undef LWS_WITH_LIBEV */
-/* #undef LWS_WITH_LIBEVENT */
-#define LWS_WITH_LIBUV
-#define LWS_WITH_LWSAC
-#define LWS_LOGS_TIMESTAMP
-/* #undef LWS_WITH_MBEDTLS */
-/* #undef LWS_WITH_MINIZ */
-#define LWS_WITH_NETWORK
-/* #undef LWS_WITH_NO_LOGS */
-#define LWS_WITH_CLIENT
-#define LWS_WITHOUT_EXTENSIONS
-#define LWS_WITH_SERVER
-/* #undef LWS_WITH_SPAWN */
-/* #undef LWS_WITH_PEER_LIMITS */
-/* #undef LWS_WITH_PLUGINS */
-/* #undef LWS_WITH_POLARSSL */
-#define LWS_WITH_POLL
-/* #undef LWS_WITH_RANGES */
-/* #undef LWS_WITH_SECURE_STREAMS */
-/* #undef LWS_WITH_SECURE_STREAMS_SYS_AUTH_API_AMAZON_COM */
-/* #undef LWS_WITH_SECURE_STREAMS_PROXY_API */
-/* #undef LWS_WITH_SECURE_STREAMS_STATIC_POLICY_ONLY */
-/* #undef LWS_WITH_SELFTESTS */
-/* #undef LWS_WITH_SEQUENCER */
-/* #undef LWS_WITH_SERVER_STATUS */
-/* #undef LWS_WITH_SYS_SMD */
-/* #undef LWS_WITH_SMTP */
-/* #undef LWS_WITH_SOCKS5 */
-/* #undef LWS_WITH_STATEFUL_URLDECODE */
-/* #undef LWS_WITH_STATS */
-/* #undef LWS_WITH_STRUCT_SQLITE3 */
-/* #undef LWS_WITH_STRUCT_JSON */
-/* #undef LWS_WITH_SUL_DEBUGGING */
-/* #undef LWS_WITH_SQLITE3 */
-/* #undef LWS_WITH_SYS_NTPCLIENT */
-/* #undef LWS_WITH_SYS_DHCP_CLIENT */
-#define LWS_WITH_SYS_STATE
-/* #undef LWS_WITH_THREADPOOL */
-#define LWS_WITH_TLS
-#define LWS_WITH_UDP
-#define LWS_WITH_UNIX_SOCK
-/* #undef LWS_WITH_ZIP_FOPS */
-/* #undef USE_OLD_CYASSL */
-/* #undef USE_WOLFSSL */
-#define LWS_WITH_EVENT_LIBS
-/* #undef LWS_WITH_EVLIB_PLUGINS */
-/* #undef LWS_WITH_LIBUV_INTERNAL */
-/* #undef LWS_WITH_PLUGINS_API */
+#define LWS_HAVE_SSL_SET_INFO_CALLBACK
+
+#define LWS_HAS_INTPTR_T
+
 
