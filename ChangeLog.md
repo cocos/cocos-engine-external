fc75425354694d06ec6d518924fb1b4f09bed4a3
timlyeee <timelyeee@gmail.com>
2021-07-16 10:26:59 +0800

    Update websocket libs and so on
    * version of lib websocket has been return to old version 2.4.2
    * freetype lib has been confirmed as static lib
    * delete unused librarys ( webpdecoder.dll, vorbisenc.dll etc )
    * rename uv dir as the old version.

---
+++ ChangeLog.md
@@ -0,0 +1,133 @@
+# External Change Log
+
+## 2021.07 new platform x64
+
+|    libs    |   win32    |    x64     |
+| :--------: | :--------: | :--------: |
+|  openssl   |   1.1.1g   |   1.1.1k   |
+| websockets |   2.4.2    |    ----    |
+|   mpg123   |     40     |     45     |
+|    curl    |   7.52.1   |   7.74.0   |
+|    png     |   1.6.37   |    ----    |
+|    sdl     |   2.0.10   |   2.0.14   |
+|    jpeg    |     9d     |    ----    |
+| freetype2  |   2.5.5    |   2.10.4   |
+|    libz    |   1.2.11   |    ----    |
+|     v8     | 6.0.286.52 |  9.1.269   |
+|   libuv    |   1.23.1   | 1.41.0-rel |
+|  sqlite3   |   3.35.4   |  3.7.15.1  |
+|    tbb     |   2020.2   |    ----    |
+| PVRVFrame  |   10.13    |    ----    |
+|   iconv    |   0x0109   |   0x0110   |
+|  glslang   |    unk     |   11.4.0   |
+|  physx   |   4.1.1    |    ----    |
+
+## Current Version
+
+* websockets:
+  * mac:        2.1.0
+  * win32:      2.4.2
+  * ios:        2.1.0
+  * android:    2.1.0
+  * win64:      2.4.2
+* openssl
+  * mac:        1.1.1g
+  * win32:      1.1.1g
+  * ios:        1.1.1g
+  * android:    1.1.1g
+  * win64:      1.1.1k
+* mpg123
+  * win32:      40
+  * win64:      45
+* curl
+  * mac:        7.52.1
+  * win32:      7.52.1
+  * win64:      7.74.0
+* png
+  * mac:        1.6.37
+  * win32:      1.6.37
+  * ios:        1.6.37
+  * android:    1.6.37
+  * win64:      1.6.37
+* sdl
+  * win32:      2.0.10
+  * win64:      2.0.14
+* jpeg
+  * mac:        9d
+  * win32:      9d
+  * ios:        9d
+  * android:    9d
+  * win64:      9d
+* freetype2
+  * mac:        2.6.3
+  * win32:      2.5.5
+  * ios:        2.6.3
+  * android:    2.6.3
+  * win64:      2.10.4
+* libz 
+  * mac:        1.2.11
+  * win32:      1.2.11
+  * ios:        --
+  * android:    1.2.11
+  * win64:      1.2.11
+* spidermonkey
+  * mac:        v52
+  * win32:      v52
+  * ios:        v52
+  * android:    v52
+  * win64:      --
+* anysdk
+  * mac:        --
+  * win32:      --
+  * android:    2.2.5
+  * ios:        2.2.5
+  * win64:      --
+* rapidjson
+  * mac:        1.1.0
+  * win32:      1.1.0
+  * ios:        1.1.0
+  * android:    1.1.0
+  * win64:      --
+* tinydir
+  * mac:        1.2.1
+  * win32:      1.2.1
+  * android:    1.2.1
+  * ios:        1.2.1
+  * win64:      --
+* v8
+  * win32:      6.0.286.52
+  * android:    6.0.286.52
+  *  win64:      9.1.269.0
+* libuv
+  * mac:        1.23.1
+  * win32:      1.13.1
+  * android:    1.13.1
+  * ios:        1.23.1
+  * win64:      1.41.0-release
+* sqlite3 
+  * win64:      3.35.4
+  * win32:      3.7.15.1
+* tbb
+  * mac:        2020.2
+  * win32:      2020.2
+  * android:    2020.2
+  * ios:        2020.2
+  * win64:      2020.2
+* PVRVFrame
+  * mac:        10.13
+  * win32:      10.13
+  * win64:      10.13
+* spirv_cross
+  * mac:        2021.1.15
+  * ios:        2021.1.15
+* iconv
+  * win32:      0x0109
+  * win64:      0x0110
+* glslang
+  * win32:
+  * win64:      11.4.0
+* physx #
+  * win32:      4.1.1
+  * win64:      4.1.1
+* glew #
+  * win64:      2.1.0
--- win64/include/freetype/fterrdef.h
+++ win64/include/freetype/fterrdef.h
@@ -1,57 +1,33 @@
-/****************************************************************************
- *
- * fterrdef.h
- *
- *   FreeType error codes (specification).
- *
- * Copyright (C) 2002-2020 by
- * David Turner, Robert Wilhelm, and Werner Lemberg.
- *
- * This file is part of the FreeType project, and may only be used,
- * modified, and distributed under the terms of the FreeType project
- * license, LICENSE.TXT.  By continuing to use, modify, or distribute
- * this file you indicate that you have read the license and
- * understand and accept it fully.
- *
- */
+/***************************************************************************/
+/*                                                                         */
+/*  fterrdef.h                                                             */
+/*                                                                         */
+/*    FreeType error codes (specification).                                */
+/*                                                                         */
+/*  Copyright 2002, 2004, 2006, 2007, 2010-2013 by                         */
+/*  David Turner, Robert Wilhelm, and Werner Lemberg.                      */
+/*                                                                         */
+/*  This file is part of the FreeType project, and may only be used,       */
+/*  modified, and distributed under the terms of the FreeType project      */
+/*  license, LICENSE.TXT.  By continuing to use, modify, or distribute     */
+/*  this file you indicate that you have read the license and              */
+/*  understand and accept it fully.                                        */
+/*                                                                         */
+/***************************************************************************/
 
 
-  /**************************************************************************
-   *
-   * @section:
-   *  error_code_values
-   *
-   * @title:
-   *  Error Code Values
-   *
-   * @abstract:
-   *  All possible error codes returned by FreeType functions.
-   *
-   * @description:
-   *  The list below is taken verbatim from the file `fterrdef.h` (loaded
-   *  automatically by including `FT_FREETYPE_H`).  The first argument of the
-   *  `FT_ERROR_DEF_` macro is the error label; by default, the prefix
-   *  `FT_Err_` gets added so that you get error names like
-   *  `FT_Err_Cannot_Open_Resource`.  The second argument is the error code,
-   *  and the last argument an error string, which is not used by FreeType.
-   *
-   *  Within your application you should **only** use error names and
-   *  **never** its numeric values!  The latter might (and actually do)
-   *  change in forthcoming FreeType versions.
-   *
-   *  Macro `FT_NOERRORDEF_` defines `FT_Err_Ok`, which is always zero.  See
-   *  the 'Error Enumerations' subsection how to automatically generate a
-   *  list of error strings.
-   *
-   */
+  /*******************************************************************/
+  /*******************************************************************/
+  /*****                                                         *****/
+  /*****                LIST OF ERROR CODES/MESSAGES             *****/
+  /*****                                                         *****/
+  /*******************************************************************/
+  /*******************************************************************/
 
 
-  /**************************************************************************
-   *
-   * @enum:
-   *   FT_Err_XXX
-   *
-   */
+  /* You need to define both FT_ERRORDEF_ and FT_NOERRORDEF_ before */
+  /* including this file.                                           */
+
 
   /* generic errors */
 
@@ -230,10 +206,6 @@
                 "invalid PostScript (post) table format" )
   FT_ERRORDEF_( Invalid_Post_Table,                          0x9B,
                 "invalid PostScript (post) table" )
-  FT_ERRORDEF_( DEF_In_Glyf_Bytecode,                        0x9C,
-                "found FDEF or IDEF opcode in glyf bytecode" )
-  FT_ERRORDEF_( Missing_Bitmap,                              0x9D,
-                "missing bitmap in strike" )
 
   /* CFF, CID, and Type 1 errors */
 
@@ -246,7 +218,7 @@
   FT_ERRORDEF_( No_Unicode_Glyph_Name,                       0xA3,
                 "no Unicode glyph name found" )
   FT_ERRORDEF_( Glyph_Too_Big,                               0xA4,
-                "glyph too big for hinting" )
+                "glyph to big for hinting" )
 
   /* BDF errors */
 
@@ -272,8 +244,6 @@
                 "Font header corrupted or missing fields" )
   FT_ERRORDEF_( Corrupted_Font_Glyphs,                       0xBA,
                 "Font glyphs corrupted or missing fields" )
-
-  /* */
 
 
 /* END */
