937d14d529a21b59fe1a2dedfe14f05614fe3c50
Yun Hsiao Wu <yunhsiaow@gmail.com>
2022-01-24 17:47:48 +0800

    better boost source cmake (#213)

--- sources/boost-source/boost.cmake
+++ sources/boost-source/boost.cmake
@@ -1,14 +1,12 @@
 
 list(APPEND CC_EXTERNAL_PRIVATE_DEFINITIONS BOOST_ALL_NO_LIB)
 
-add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/container boost/container)
-
-set(BOOST_LIBS
-    boost_container
+set(BOOST_LIB_NAMES
+    container
 )
 
-foreach(lib ${BOOST_LIBS})
-    set_target_properties(${lib} PROPERTIES FOLDER Utils)
+foreach(lib ${BOOST_LIB_NAMES})
+    add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/${lib} boost/${lib})
+    set_target_properties(boost_${lib} PROPERTIES FOLDER Utils)
+    list(APPEND CC_EXTERNAL_LIBS boost_${lib})
 endforeach()
-
-list(APPEND CC_EXTERNAL_LIBS ${BOOST_LIBS})
