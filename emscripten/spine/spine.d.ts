abafba42f1594f7067b1dbee6295d7c252ced010
pp_pro <chinese1236@qq.com>
2023-06-21 16:04:07 +0800

    add wasm.fallback module

--- emscripten/spine/spine.d.ts
+++ emscripten/spine/spine.d.ts
@@ -6,6 +6,10 @@ declare module 'external:emscripten/spine/spine.wasm.js' {
     export default SpineWasm;
 }
 
+declare module 'external:emscripten/spine/spine.wasm.fallback.js' {
+    export default SpineWasm;
+}
+
 // tslint:disable
 declare function SpineWasm (moduleOptions?: any): Promise<void>;
 
