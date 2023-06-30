b570f4754ad6650a3f45194f12f3e39a15de6c6e
liqiao <liqiao@cocos.com>
2023-06-30 14:41:55 +0800

    update spine wasm

--- emscripten/spine/spine.d.ts
+++ emscripten/spine/spine.d.ts
@@ -6,10 +6,6 @@ declare module 'external:emscripten/spine/spine.wasm.js' {
     export default SpineWasm;
 }
 
-declare module 'external:emscripten/spine/spine.wasm.fallback.js' {
-    export default SpineWasm;
-}
-
 // tslint:disable
 declare function SpineWasm (moduleOptions?: any): Promise<void>;
 
