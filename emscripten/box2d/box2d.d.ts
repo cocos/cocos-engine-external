b47943dd2d55615087baa366efc4bd7714b40bb2
Ling Zhan <lealzhan@126.com>
2023-08-17 14:18:11 +0800

    add box2d-wasm (#397)

---
+++ emscripten/box2d/box2d.d.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./b2.d.ts" />
+
+declare module 'external:emscripten/box2d/box2d.release.asm.js' {
+    export default BOX2D;
+}
+
+declare module 'external:emscripten/box2d/box2d.release.wasm.js' {
+    export default BOX2D;
+}
+
+// tslint:disable
+declare function BOX2D (moduleOptions?: any): Promise<void>;
