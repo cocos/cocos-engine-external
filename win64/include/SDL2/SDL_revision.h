fdb32feba4147844a94476ed3c861dc8d0da31d5
qiuguohua <qiuguohua111@126.com>
2023-08-18 14:42:08 +0800

    Configure sdl interface to remove unused modules

--- win64/include/SDL2/SDL_revision.h
+++ win64/include/SDL2/SDL_revision.h
@@ -1,2 +1,8 @@
-#define SDL_REVISION "hg-14525:e52d96ea04fc"
-#define SDL_REVISION_NUMBER 14525
+/* #undef SDL_VENDOR_INFO */
+#define SDL_REVISION_NUMBER 0
+
+#ifdef SDL_VENDOR_INFO
+#define SDL_REVISION "SDL-2.28.1-no-vcs (" SDL_VENDOR_INFO ")"
+#else
+#define SDL_REVISION "SDL-2.28.1-no-vcs"
+#endif
