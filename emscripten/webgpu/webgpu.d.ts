00e4b6c23a773edc33021ad4d4bc63a208af65a3
Zeqiang Li <zeqiang-li@outlook.com>
2023-09-08 14:15:26 +0800

    update webgpu (#417)

--- emscripten/webgpu/webgpu.d.ts
+++ emscripten/webgpu/webgpu.d.ts
@@ -7,3 +7,8 @@ declare module 'external:emscripten/webgpu/glslang.js' {
     function factory (wasmUrl: string): Promise<any>;
     export default factory;
 }
+
+declare module 'external:emscripten/webgpu/twgsl.js' {
+    function factory (gfx: any): Promise<any>;
+    export default factory;
+}
